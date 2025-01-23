34eb27f9cb21b02d2812f49493b3f4dd84e3db65
Denys Kibalenko <dennieking87@gmail.com>
2025-01-23 09:03:40 +0100

    Feat: add `BorrowingList`, `BorrowingDetail` views to borrowings/views.py & url endpoints for them to borrowings/urls.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+
+
+class BorrowingList(generics.ListAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+
+class BorrowingDetail(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer

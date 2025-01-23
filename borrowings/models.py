ca99821da90fe2b061d9f1c30b99beb0524f4b21
Denys Kibalenko <dennieking87@gmail.com>
2025-01-23 08:47:22 +0100

    Feat: add `Borrowing` model with constraints for `borrow_date`, `expected_return_date`, `actual_return_date` to borrowings/models.py
    - First constraint ensures that `borrow_date` is less than of equals to `expected_return_date`
    - Second constraint ensures that `actual_return_date` is greater than or equals to `borrow_date` or `actual_return_date` is null meaning a book is still borrowed

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.db.models import Q, F, CheckConstraint
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return f"{self.book.title} borrowed by {self.user.email}"
+
+    class Meta:
+        ordering = ["borrow_date"]
+        constraints = [
+            CheckConstraint(
+                check=Q(borrow_date__lte=F("expected_return_date")),
+                name="check_borrow_date_lte_expected_return_date",
+            ),
+            CheckConstraint(
+                check=Q(actual_return_date__gte=F("borrow_date"))
+                | Q(actual_return_date__isnull=True),
+                name="check_actual_return_date_gte_borrow_date_or_null",
+            ),
+        ]

1a3a594290acdb598b6fa47a124301b9badf8266
Denys Kibalenko <dennieking87@gmail.com>
2025-01-28 19:25:15 +0100

    Feat: add `BorrowingReturnView` to serve borrowing return endpoint to borrowings/views.py & add url route under `<int:pk>/return/` prefix to borrowings/urls.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,8 +1,17 @@
+from django.db import transaction
+from django.utils import timezone
+
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingDetailSerializer,
+    BorrowingReturnSerializer
+)
 
 
 class BorrowingListView(generics.ListCreateAPIView):
@@ -34,5 +43,26 @@ class BorrowingListView(generics.ListCreateAPIView):
 
 class BorrowingDetailView(generics.RetrieveAPIView):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingSerializer
+    serializer_class = BorrowingDetailSerializer
     permission_classes = [IsAuthenticated]
+
+
+class BorrowingReturnView(generics.GenericAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReturnSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        borrowing = self.get_object()
+        serializer = self.get_serializer(borrowing)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk, forma=None):
+        with transaction.atomic():
+            borrowing = self.queryset.get(pk=pk)
+            borrowing.actual_return_date = timezone.now().date()
+            borrowing.book.inventory += 1
+            borrowing.book.save()
+            borrowing.save()
+        serializer = self.get_serializer(borrowing)
+        return Response(serializer.data)
